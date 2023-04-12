ec5df57b511de11931f3f980af972b476974384e
Erick <erick.saltosmo@gmail.com>
2023-04-12 18:02:28 -0500

    update segundo cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { LoadingComponent } from './Components/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule
