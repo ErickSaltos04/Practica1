43c7f2263b90b455ac57510ab922cdabd795e5dc
Erick <erick.saltosmo@gmail.com>
2023-04-14 18:53:19 -0500

    clase viernes

--- src/app/Pages/inicio/inicio.component.ts
+++ src/app/Pages/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -10,10 +11,18 @@ nombre = 'Erick';
 apellido = 'Saltos';
 loadingVisible = false;
 
+constructor(private ruta: Router ){
+
+}
+
 visualizarLoading(){
   this.loadingVisible = true;
   setTimeout(()=>{
     this.loadingVisible = false
   },1000);
+}
+
+irPaginaTabla(){
+this.ruta.navigate(['tabla']);
 }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './Pages/inicio/inicio.component';
 import { LoadingComponent } from './Components/loading/loading.component';
-
+import { ButtonModule } from 'primeng/button';
 import{AppRoutingModule} from './app-routing.module';
+import { PaginaTablaComponent } from './Pages/pagina-tabla/pagina-tabla.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    LoadingComponent
+    LoadingComponent,
+    PaginaTablaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
