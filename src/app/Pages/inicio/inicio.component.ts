74fe6123a95e97c4a78bd2b6913a25169f42f43e
Erick <erick.saltosmo@gmail.com>
2023-04-21 20:58:42 -0500

    Clase 04/21

--- src/app/Pages/inicio/inicio.component.ts
+++ src/app/Pages/inicio/inicio.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-inicio',
@@ -12,10 +13,11 @@ export class InicioComponent implements OnInit{
 nombre = 'Erick';
 apellido = 'Saltos';
 loadingVisible = false;
-formUsurio!: FormGroup;
+formEmpleado!: FormGroup;
 
 constructor(private ruta: Router,
-  private mensajes: MessageService ){
+  private mensajes: MessageService,
+  private serviceEmpleado: EmpleadoService ){
 
 }
   ngOnInit(): void {
@@ -34,19 +36,38 @@ this.ruta.navigate(['tabla']);
 }
 
 verificar(){
-  console.log(this.formUsurio)
-  if(this.formUsurio.valid){
-    this.mensajes.add({ severity: 'success', summary: 'Satisfactorio', detail: 'Exito' });
+  console.log(this.formEmpleado)
+  if(this.formEmpleado.valid){
+    this.crearEmpleado();
   } else {
     this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Hubo un problema' });
   };
 }
 
+crearEmpleado(){
+this.serviceEmpleado.
+createEmployee( this.formEmpleado.value.name,
+                this.formEmpleado.value.salary, 
+                this.formEmpleado.value.age).subscribe({
+                    next: (resp) =>{
+                      console.log(resp);
+                      this.formEmpleado.reset();
+                      this.mensajes.add({ severity: 'success', summary: 'Empleado creado con éxito', detail: 'Exito' });
+                    },
+                    error: (resp) => {
+                      console.log(resp);
+                      this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Hubo un error' });
+                    }
+                  }
+                );
+}
+
 buildFormulario(){
-  this.formUsurio = new FormGroup(
+  this.formEmpleado = new FormGroup(
     {
-      nombre: new FormControl('', [Validators.required]),
-      apellido: new FormControl('', [Validators.required])
+      name: new FormControl('', [Validators.required]),
+      salary: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required])
     }
   )
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { iEmpleados } from '../interfaces/empleadosInterfaces';
+import { Header } from 'primeng/api';
 const API_GET_ALL_EMPLOYEE = environment.API_GET_ALL_EMPLOYEE;
+const API_POST_CREATE_EMPLOYEE = environment.API_POST_CREATE_EMPLOYEE;
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +12,27 @@ export class EmpleadoService {
 
   constructor(private http: HttpClient) { }
 
+  createHeader(){
+    let headers: HttpHeaders;
+      headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8')
+      .set('Acces-Control-Allow_Origin', '*')
+    return headers;
+  }
+
   getAllEmployee(){
     return this.http.get<iEmpleados>(API_GET_ALL_EMPLOYEE);
   }
+
+  createEmployee(name: string, salary: string, age: string){
+    //const headers = this.createHeader();
+    let body = {
+      name: name,
+      salary: salary,
+      age: age 
+    };
+    //Para uso de headers
+    //return this.http.post(API_POST_CREATE_EMPLOYEE, body,{headers});
+    return this.http.post(API_POST_CREATE_EMPLOYEE, body);
+  }
+
 }
